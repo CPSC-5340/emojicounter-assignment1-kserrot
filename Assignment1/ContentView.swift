19efa645e4e183b87d01709fca60cd18315e7d16
Kaique Torres <kaique.serrot@gmail.com>
2024-03-17 20:28:39 -0500

    Assigment 1/ 95% Done

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    struct EmojiCounter: Identifiable {
+        let id = UUID()
+        var emoji: String
+        var count: Int
+    }
+    
+    @State private var emojiCounters = [
+        EmojiCounter(emoji: "😂", count: 0),
+        EmojiCounter(emoji: "😉", count: 0),
+        EmojiCounter(emoji: "😆", count: 0)
+    ]
+    
+    @State private var lastButtonPressTime: Date? = nil
+    
+    let simultaneousPressThreshold: TimeInterval = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach($emojiCounters) { $emojiCounter in
+                    HStack {
+                        Text(emojiCounter.emoji)
+                            .font(.largeTitle)
+                        Spacer()
+                        Text("Counter: \(emojiCounter.count)")
+                        Spacer()
+                        Button(action: {
+                            print("Plus tapped for \($emojiCounter.wrappedValue.emoji)")
+                            self.handleButtonPress {
+                                emojiCounter.count += 1
+                            }
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.green)
+                        }
+                        Button(action: {
+                            print("Minus tapped for \($emojiCounter.wrappedValue.emoji)")
+                            self.handleButtonPress {
+                                emojiCounter.count -= 1
+                            }
+                        }) {
+                            Image(systemName: "minus.square.fill")
+                                .foregroundColor(.red)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Emoji Counter")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+    }
+    
+    private func handleButtonPress(action: @escaping () -> Void) {
+        let now = Date()
+        if let lastPressTime = lastButtonPressTime, now.timeIntervalSince(lastPressTime) <= simultaneousPressThreshold {
+            print("Simultaneous press detected")
+        } else {
+            action()
+        }
+        lastButtonPressTime = now
     }
 }
 
